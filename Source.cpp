ef27aa00fcf2a3bec78773de99e2fac2614f3d0d
kenji <2605401+kenjinote@users.noreply.github.com>
2023-02-11 01:07:31 +0900

    initial commit

---
+++ Source.cpp
@@ -0,0 +1,69 @@
+#pragma comment(lib, "shlwapi")
+#include <windows.h>
+#include <shlwapi.h>
+#include <shlobj.h>
+
+TCHAR szClassName[] = TEXT("Window");
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_CREATE:
+	{
+		TCHAR szPath[MAX_PATH];
+		TCHAR szUrl[MAX_PATH];
+		DWORD dwSize = MAX_PATH;
+		SHGetSpecialFolderPath(0, szPath, CSIDL_DESKTOP, 0);
+		UrlCreateFromPath(szPath, szUrl, &dwSize, NULL);
+		MessageBox(hWnd, szUrl, TEXT("デスクトップのURL"), 0);
+		return -1;
+	}
+	break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, msg, wParam, lParam);
+	}
+	return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInst, LPSTR pCmdLine, int nCmdShow)
+{
+	MSG msg;
+	WNDCLASS wndclass = {
+		CS_HREDRAW | CS_VREDRAW,
+		WndProc,
+		0,
+		0,
+		hInstance,
+		0,
+		LoadCursor(0, IDC_ARROW),
+		(HBRUSH)(COLOR_WINDOW + 1),
+		0,
+		szClassName
+	};
+	RegisterClass(&wndclass);
+	HWND hWnd = CreateWindow(
+		szClassName,
+		TEXT("Window"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		0,
+		CW_USEDEFAULT,
+		0,
+		0,
+		0,
+		hInstance,
+		0
+		);
+	ShowWindow(hWnd, SW_SHOWDEFAULT);
+	UpdateWindow(hWnd);
+	while (GetMessage(&msg, 0, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return msg.wParam;
+}
